c5c7876c6783db8a8d51f79dca313f8521897ff1
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-09 21:20:56 +0600

    New route added.

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,7 @@ export default [
   layout("./layouts/MainLayout.tsx", [
     index("./routes/home.tsx"),
     route("about", "./routes/about.tsx"),
+    route("projects", "./routes/projects.tsx"),
     // route("login", "./auth/login.tsx"),
   ]),
 ] satisfies RouteConfig;
